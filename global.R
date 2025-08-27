b88a18f912e478d3c549fc52ffb0821bb3d48c8d
Breno C R Silva <122389486+csilv7@users.noreply.github.com>
2025-08-27 18:49:27 -0300

    Add files via upload

---
+++ global.R
@@ -0,0 +1,22 @@
+# --------------------------
+# [1] CONFIGURAÇÕES INICIAIS
+# --------------------------
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(bs4Dash)
+
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+
+library(DT)
+
+library(ggplot2)
+library(plotly)
+library(echarts4r)
+
+library(htmlwidgets)
+
+library(leaflet)
+library(sf)
